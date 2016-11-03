bcd5472979096599f3bdcf457be467f571ec96cf
chubflat <khasebe1115@gmail.com>
2016-11-03 15:23:15 +0900

    C#

---
+++ Assets/Scripts/settingManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class settingManager : MonoBehaviour {
+
+	public InputField nameField;
+	public InputField roomIdField;
+	public InputField ipAddressField;
+	public InputField portField;
+	public Text clientIdText;
+
+	public void onClick(){
+		PlayerPrefs.SetString("name",nameField.text);
+		PlayerPrefs.SetString("roomId",roomIdField.text);
+		PlayerPrefs.SetString("ipAddress",ipAddressField.text);
+		PlayerPrefs.SetString("port",portField.text);
+	}
+
+	// Use this for initialization
+	void Start () {
+		// getField
+		if(PlayerPrefs.HasKey("name")){
+			nameField.text = PlayerPrefs.GetString("name");
+		}
+
+		if(PlayerPrefs.HasKey("roomId")){
+			roomIdField.text = PlayerPrefs.GetString("roomId");
+		}
+
+		if(PlayerPrefs.HasKey("ipAddress")){
+			ipAddressField.text = PlayerPrefs.GetString("ipAddress");
+		}
+
+		if(PlayerPrefs.HasKey("port")){
+			portField.text = PlayerPrefs.GetString("port");
+		}
+
+		if(PlayerPrefs.HasKey("clientId")){
+			clientIdText.text = PlayerPrefs.GetString("clientId");
+		}else{
+			clientIdText.text = utility.createClientId();// 初回起動時にclientIdをcreate
+			PlayerPrefs.SetString("clientId",clientIdText.text);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/utility.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class utility : MonoBehaviour {
+
+	public static string createClientId(){
+		string c = "abcdefghijklmnopqrstrvwxyz0123456789";
+		int cl = c.Length;
+		string clientId ="";
+		for(int i = 0; i < 16;i++){
+			clientId += c.Substring(Random.Range(0,cl-1),1);
+		}
+		return clientId;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
