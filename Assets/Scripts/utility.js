1de7ed69b182b4723c09522df7ae48cc0d0c1d35
chubflat <khasebe1115@gmail.com>
2016-11-03 14:09:56 +0900

    settingStatus の保存

---
+++ Assets/Scripts/utility.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+import UnityEngine;
+static function createClientId(){
+
+	var c = "abcdefghijklmnopqrstrvwxyz0123456789";
+	var cl = c.length;
+	var clientId ="";
+	for(var i = 0; i < 16;i++){
+		clientId += c[Random.Range(0,cl-1)];
+	}
+	return clientId;
+}
+
+
+function Start () {
+
+}
+
+function Update () {
+
+}
