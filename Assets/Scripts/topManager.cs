d914db2c4548dc09e8df09262d3988322debaad3
chubflat <khasebe1115@gmail.com>
2016-11-19 00:06:06 +0900

    <topManager> fix settingButton

---
+++ Assets/Scripts/topManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class topManager : MonoBehaviour {
+
+	public void onClick(){
+		SceneManager.LoadScene ("setting");
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
