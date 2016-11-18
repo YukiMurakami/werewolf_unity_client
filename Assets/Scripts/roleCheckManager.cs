c9cd6e6a8743faa749323e8df57743c26ee8cf3a
chubflat <khasebe1115@gmail.com>
2016-11-19 01:34:29 +0900

    fix roleCheckScene

--- Assets/Scripts/roleCheckManager.cs
+++ Assets/Scripts/roleCheckManager.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class roleCheckManager : MonoBehaviour {
 
+	public void onClicktoChat(){ //チャットシーンへ
+		SceneManager.LoadScene ("chat");
+	}
 	// Use this for initialization
 	void Start () {
 	
