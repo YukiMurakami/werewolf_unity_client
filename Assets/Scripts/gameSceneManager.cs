a35d7bad159fa432705b1fdcd3c7eeff4bbef705
chubflat <khasebe1115@gmail.com>
2017-07-02 08:58:10 +0900

    GameSceneManagerの修正

--- Assets/Scripts/gameSceneManager.cs
+++ Assets/Scripts/gameSceneManager.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class gameSceneManager : MonoBehaviour {
 	public Text timeText;
 	public Text phaseText;
 	public Text resultText;
-	
+
 	public enum Scene{
 		morning,
 		afternoon,
@@ -17,10 +19,28 @@ public class gameSceneManager : MonoBehaviour {
 		heaven,
 		result,
 	}
+	public void onClicktoAfternoon(){
+		// SceneManager.LoadScene ("room");
+	}
+	public void onClicktoEvening(){
+		// SceneManager.LoadScene ("room");
+	}
+	public void onClicktoNight(){
+		// SceneManager.LoadScene ("room");
+	}
+	public void onClicktoVotingHistory(){
 
+	}
+	public void onClicktoVictimHistory(){
+
+	}
 
 	// Use this for initialization
 	void Start () {
+		// dayを取得する
+		int day = 3;
+		resultText.text = System.String.Format("{0}日目の朝になりました。昨晩の犠牲者はplayerDでした。（昨晩のアクション：playerBがplayerDをかみました）",day);
+		phaseText.text = day + "日目朝";
 		
 	}
 	
